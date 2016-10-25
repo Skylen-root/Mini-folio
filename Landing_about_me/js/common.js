b25cfa1b0763c9fcfc9b0dbc7be0410c112e78c1
Skylen-root <pacman.root@gmail.com>
2016-10-25 23:18:14 +0300

    Новий лендінг Landing_about_me
    Додано новий лендінг Landing_about_me.
    Потрібно заповнити секції, та додати анімацію

---
+++ Landing_about_me/js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	//resize height
+	function heightDetect(){
+		$(".main_head").css("height", $(window).height());
+	}
+	heightDetect();
+	$(window).resize(function(){
+		heightDetect();
+	})
+	//
+
+//.toggle_menu
+	$(".toggle_menu").click(function(){
+		if ($(".top_menu").is(":visible")) {
+			$(".top_menu").fadeOut(600);
+		}
+
+
+		else {
+		$(".top_menu").fadeIn(600);
+		};
+	});
+//
+
+$(".toggle_menu, .menu_item").click(function() {
+  $(".sandwich").toggleClass("active");
+});
+
+
+
+});
