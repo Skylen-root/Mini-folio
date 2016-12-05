542f3f234f59a7decd8d2158cf1e97dd40bb2b78
Skylen-root <pacman.root@gmail.com>
2016-12-05 18:48:35 +0200

    Контакти
    Заповник секцію Контакти
    додав форму зворотнього зв'язку
    
    Додано нову сторінку - Examen

--- Landing_about_me/js/common.js
+++ Landing_about_me/js/common.js
@@ -42,4 +42,26 @@ $(".toggle_menu, .menu_item").click(function() {
 
 
 
+
+
+
+
+
+
+
+
+// contact form
+$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+//
 });
