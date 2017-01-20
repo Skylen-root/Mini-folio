2c5d27c7908ae3a08e506614269be2a1ee562c59
Skylen-root <pacman.root@gmail.com>
2017-01-20 18:08:33 +0200

    landing - додано  карусель

--- Landing/js/common.js
+++ Landing/js/common.js
@@ -1,9 +1,41 @@
-$( window ).resize(function() {
-	$(this).css({'height' : '100%'});
 
 
 
+$('.user_slider').slick({
+	dots: true,
+	infinite: true,
+ 	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true
 });
 
 
+$('.brand_slider').slick({
+		infinite: true,
+		// adaptiveHeight: true,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		 responsive: [{
+
+      breakpoint: 1650,
+      settings: {
+        slidesToShow: 4,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 4,
+      }
+
+    }, {
+
+      breakpoint: 878,
+      settings: {
+        slidesToShow: 2,
+         }
+    }]
+	});
 
